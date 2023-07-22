54a7ed257b585ea3611a405a64f88a0b535a437b
ForkEyeee <shawncarter123456@gmail.com>
2023-07-22 17:16:05 -0400

    working on projects component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Header from "./components/header";
-import Projects from "./components/ProjectsItem";
+import Projects from "./components/Projects";
 function App() {
   return (
     <div>
---
+++ src/components/ProjectList.jsx
@@ -0,0 +1,16 @@
+// import Project from "./Project";
+
+// function ProjectList() {
+//   return (
+//     <div>
+//       <Project />
+//       <Project />
+//       <Project />
+//       <Project />
+//       <Project />
+//       <Project />
+//     </div>
+//   );
+// }
+
+// export default ProjectList;
